f49c9c0e46102abc2a2225971ee9ab4c9be49711
Douglas Alves <doug-pr@hotmail.com>
2022-11-09 20:20:40 -0300

    Adicionando EnderecoController e PessoaController

---
+++ src/main/java/com/barcelos/projetointegrador/controllers/EnderecoController.java
@@ -0,0 +1,41 @@
+package com.barcelos.projetointegrador.controllers;
+
+import com.barcelos.projetointegrador.models.Endereco;
+import com.barcelos.projetointegrador.services.EnderecoServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin
+public class EnderecoController {
+    final EnderecoServiceImpl enderecoServiceImpl;
+
+    public EnderecoController(EnderecoServiceImpl enderecoService){
+        enderecoServiceImpl = enderecoService;
+    }
+
+    @PostMapping(value = "/salvarEndereco")
+    public ResponseEntity<Object>salvarEndereco(@RequestBody Endereco endereco){
+        Endereco response = enderecoServiceImpl.salvar(endereco);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @GetMapping(value = "/buscarEndereco")
+    public ResponseEntity<Object>buscarEndereco(){
+        List<Endereco> response = enderecoServiceImpl.listar();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @PostMapping(value = "/alterarEndereco")
+    public ResponseEntity<Object>alterarEndereco(Endereco endereco){
+        Endereco response = enderecoServiceImpl.editar(endereco);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @DeleteMapping(value = "/deletarEndereco")
+    public ResponseEntity<Object>deletarEndereco(Long id_endereco){
+        enderecoServiceImpl.deletar(id_endereco);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
---
+++ src/main/java/com/barcelos/projetointegrador/controllers/PessoaController.java
@@ -0,0 +1,40 @@
+package com.barcelos.projetointegrador.controllers;
+
+import com.barcelos.projetointegrador.models.Pessoa;
+import com.barcelos.projetointegrador.services.PessoaServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin
+public class PessoaController {
+    final PessoaServiceImpl pessoaServiceImpl;
+
+    public PessoaController(PessoaServiceImpl pessoaService){
+        this.pessoaServiceImpl = pessoaService;
+    }
+    @PostMapping(value = "/salvarPessoa")
+    public ResponseEntity<Object>salvarPessoa(@RequestBody Pessoa pessoa){
+        Pessoa response = pessoaServiceImpl.salvar(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @GetMapping(value = "/buscarPessoa")
+    public ResponseEntity<Object>buscarPessoa(){
+        List<Pessoa> response = pessoaServiceImpl.listar();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @PutMapping(value = "alterarPessoa")
+    public ResponseEntity<Object>alterarPessoa(@RequestBody Pessoa pessoa){
+        Pessoa response = pessoaServiceImpl.editar(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @DeleteMapping(value = "deletarPessoa")
+    public ResponseEntity<Object>deletarPessoa(Long id_pessoa){
+        pessoaServiceImpl.deletar(id_pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
